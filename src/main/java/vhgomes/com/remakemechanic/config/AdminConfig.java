841cf0f42101de09dbce0e3ac0f92e871ee49b5b
Victor Hugo <vhclaudiogomes@gmail.com>
2024-04-15 15:41:39 -0300

    adminconfig and auth and docker compose

---
+++ src/main/java/vhgomes/com/remakemechanic/config/AdminConfig.java
@@ -0,0 +1,48 @@
+package vhgomes.com.remakemechanic.config;
+
+import jakarta.transaction.Transactional;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import vhgomes.com.remakemechanic.models.Role;
+import vhgomes.com.remakemechanic.models.User;
+import vhgomes.com.remakemechanic.repositories.RoleRepository;
+import vhgomes.com.remakemechanic.repositories.UserRepository;
+
+import java.util.Set;
+
+@Configuration
+public class AdminUserConfig implements CommandLineRunner {
+
+    private final RoleRepository roleRepository;
+    private final UserRepository userRepository;
+    private final BCryptPasswordEncoder bCryptPasswordEncoder;
+
+    public AdminUserConfig(RoleRepository roleRepository, UserRepository userRepository, BCryptPasswordEncoder bCryptPasswordEncoder) {
+        this.roleRepository = roleRepository;
+        this.userRepository = userRepository;
+        this.bCryptPasswordEncoder = bCryptPasswordEncoder;
+    }
+
+    @Override
+    @Transactional
+    public void run(String... args) throws Exception {
+        var roleAdmin = roleRepository.findByName(Role.Values.ADMIN.name());
+        var userAdmin = userRepository.findByUsername("admin");
+
+        userAdmin.ifPresentOrElse(
+                (user) -> {
+                    System.out.println("Admin já existe");
+                },
+                () -> {
+                    var user = new User();
+                    user.setUsername("admin");
+                    user.setPassword(bCryptPasswordEncoder.encode("123"));
+                    user.setRole(Set.of(roleAdmin));
+                    user.setName("Victor Hugo Cláudio Gomes");
+                    userRepository.save(user);
+                }
+        );
+
+    }
+}
