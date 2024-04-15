1cd1c75066c46ce8f70a5eb0d8085a78a13c6580
Victor Hugo <vhclaudiogomes@gmail.com>
2024-04-15 15:06:22 -0300

    initial commit

---
+++ src/test/java/vhgomes/com/remakemechanic/RemakeMechanicApplicationTests.java
@@ -0,0 +1,13 @@
+package vhgomes.com.remakemechanic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RemakeMechanicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
